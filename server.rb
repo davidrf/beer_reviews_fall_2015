b510970fe470223860ce35dfdb232ba144e261bf
David Rodriguez Fuentes <davidrf09@gmail.com>
2015-08-27 17:22:57 -0400

    Refactored branch

--- server.rb
+++ server.rb
@@ -11,46 +11,69 @@ def db_connection
   end
 end
 
-def beer_all
-  db_connection do |conn|
-    sql_query = "SELECT * FROM beers"
-    conn.exec(sql_query)
+class Beer
+  attr_accessor :id, :name
+  def initialize(id=nil, name)
+    @id = id
+    @name = name
   end
-end
 
-def beer_save(params)
-  unless params["name"].empty?
+  def self.all
+    beers = nil
     db_connection do |conn|
-      sql_query = "INSERT INTO beers (name) VALUES ($1)"
-      data = [params["name"]]
-      conn.exec_params(sql_query, data)
+      sql_query = "SELECT * FROM beers"
+      beers = conn.exec(sql_query)
+    end
+    beers.map { |beer| Beer.new(beer["id"], beer["name"]) }
+  end
+
+  def self.find(id)
+    beer = nil
+    db_connection do |conn|
+      sql_query = "SELECT * FROM beers WHERE id = $1"
+      data = [id]
+      beer = conn.exec_params(sql_query, data).first
+    end
+    Beer.new(beer["id"], beer["name"])
+  end
+
+  def save
+    unless name.empty?
+      db_connection do |conn|
+        sql_query = "INSERT INTO beers (name) VALUES ($1)"
+        data = [name]
+        conn.exec_params(sql_query, data)
+      end
     end
   end
-end
 
-def beer_find(id)
-  db_connection do |conn|
-    sql_query = "SELECT * FROM beers WHERE id = $1"
-    data = [id]
-    conn.exec_params(sql_query, data).first
+  def reviews
+    reviews = nil
+    db_connection do |conn|
+      sql_query = "SELECT beers.*, reviews.* FROM beers
+      JOIN reviews ON beers.id = reviews.beer_id
+      WHERE beers.id = $1"
+      data = [id]
+      reviews = conn.exec_params(sql_query, data)
+    end
+    reviews.map { |review| Review.new(review["id"], review["body"], review["beer_id"]) }
   end
 end
 
-def beer_reviews(id)
-  db_connection do |conn|
-    sql_query = "SELECT beers.*, reviews.* FROM beers
-    JOIN reviews ON beers.id = reviews.beer_id
-    WHERE beers.id = $1"
-    data = [id]
-    conn.exec_params(sql_query, data)
+class Review
+  attr_accessor :id, :body, :beer_id
+  def initialize(id=nil, body, beer_id)
+    @id = id
+    @body = body
+    @beer_id = beer_id
   end
-end
 
-def review_save(params)
-  db_connection do |conn|
-    sql_query = "INSERT INTO reviews (body, beer_id) VALUES ($1, $2)"
-    data = [params["body"], params["id"]]
-    conn.exec_params(sql_query, data)
+  def save
+    db_connection do |conn|
+      sql_query = "INSERT INTO reviews (body, beer_id) VALUES ($1, $2)"
+      data = [body, beer_id]
+      conn.exec_params(sql_query, data)
+    end
   end
 end
 
@@ -59,22 +82,24 @@ get "/" do
 end
 
 get "/beers" do
-  beers = beer_all
+  beers = Beer.all
   erb :'beers/index', locals: { beers: beers }
 end
 
 post "/beers" do
-  beer_save(params)
+  beer = Beer.new(params[:name])
+  beer.save
   redirect "/beers"
 end
 
 get "/beers/:id" do
-  beer = beer_find(params[:id])
-  reviews = beer_reviews(params[:id])
+  beer = Beer.find(params[:id])
+  reviews = beer.reviews
   erb :'beers/show', locals: { beer: beer, reviews: reviews}
 end
 
-post "/beers/:id/reviews" do
-  review_save(params)
-  redirect "/beers/" + params[:id]
+post "/beers/:beer_id/reviews" do
+  review = Review.new(params[:body], params[:beer_id])
+  review.save
+  redirect "/beers/" + review.beer_id
 end
